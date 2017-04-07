1b849a1936d80d87676152d5f5f7c2db11800f07
yaofeng <yaofeng@sodao.com>
2017-04-07 14:14:43 +0800

    consumer

---
+++ src/WMSoft.ActiveMq/Consumer/Base/Consumer.cs
@@ -0,0 +1,39 @@
+﻿using Apache.NMS;
+using WMSoft.ActiveMq.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WMSoft.ActiveMq
+{
+    /// <summary>
+    /// 消息队列生产者
+    /// </summary>
+    public class Consumer
+    {
+        ServiceConfig _config = null;
+        internal Consumer(string name)
+        {
+            _config = SectionController.Default.GetConfig(name);
+        }
+
+        public virtual void Start(Action<IMessage> callback = null)
+        {
+            try
+            {
+                var connection = ConnectionPool.GetConnection(_config.Name, _config.ActiveMQUri);
+                connection.Start();
+
+                var session = connection.CreateSession();
+                var consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(_config.TopicOrQueueName), null);
+                if (callback != null)
+                    consumer.Listener += new MessageListener(callback);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
